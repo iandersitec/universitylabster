514a3ac6658e2013558ff9acad8216a99b714ea4
Lennox97 <lennox.samp@yahoo.com>
2017-12-08 22:58:45 +0200

    asdasd

--- UniversityLabster/app/src/main/java/ro/ianders/universitylabster/MainActivity.java
+++ UniversityLabster/app/src/main/java/ro/ianders/universitylabster/MainActivity.java
@@ -1,26 +1,23 @@
 package ro.ianders.universitylabster;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
-
 public class MainActivity extends AppCompatActivity {
 
 
-	private LinearLayout linStudentiTabBar;
+    private LinearLayout linStudentiTabBar;
     private ImageButton btnTabAstaziNoutati;
     private ImageButton btnTabSaptamanaNoutati;
     private ImageButton btnTabLinkuri;
+
     private BottomNavigationView.OnNavigationItemSelectedListener
             mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -35,21 +32,36 @@ public class MainActivity extends AppCompatActivity {
                      manager.beginTransaction()
                              .replace(R.id.linFragments, new NoutatiFragment())
                              .commit();
+
+                     linStudentiTabBar.setVisibility(View.VISIBLE);
+
                     return true;
                 case R.id.navigationProfil:
                     manager.beginTransaction()
                             .replace(R.id.linFragments, new ProfilFragment())
                             .commit();
+
+                    if(linStudentiTabBar.getVisibility() == View.VISIBLE)
+                        linStudentiTabBar.setVisibility(View.GONE);
+
                     return true;
                 case R.id.navigationOrar:
                     manager.beginTransaction()
                             .replace(R.id.linFragments, new OrarFragment())
                             .commit();
+
+                    if(linStudentiTabBar.getVisibility() == View.VISIBLE)
+                        linStudentiTabBar.setVisibility(View.GONE);
+
                     return true;
                 case R.id.navigationStudenti:
                     manager.beginTransaction()
                             .replace(R.id.linFragments, new StudentiFragment())
                             .commit();
+
+                    if(linStudentiTabBar.getVisibility() == View.VISIBLE)
+                        linStudentiTabBar.setVisibility(View.GONE);
+
                     return true;
 
             }
@@ -63,12 +75,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-		
+        linStudentiTabBar = findViewById(R.id.linTabStudenti);
+        btnTabAstaziNoutati = findViewById(R.id.btnNoutatiAstazi);
+        btnTabSaptamanaNoutati = findViewById(R.id.btnNoutatiSaptamana);
+        btnTabLinkuri = findViewById(R.id.btnLinkuriPrimite);
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         navigation.setSelectedItemId(0);
+
+
     }
 
 }
