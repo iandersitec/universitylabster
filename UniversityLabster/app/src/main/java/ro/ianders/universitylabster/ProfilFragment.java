8da0bb092add8a680632b6ab6987d8ae9e57caee
Lennox97 <lennox.samp@yahoo.com>
2017-12-10 01:55:15 +0200

    save edit fifty

--- UniversityLabster/app/src/main/java/ro/ianders/universitylabster/ProfilFragment.java
+++ UniversityLabster/app/src/main/java/ro/ianders/universitylabster/ProfilFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -23,13 +24,14 @@ public class ProfilFragment extends Fragment {
     private FirebaseAuth firebaseAuth;
     private Button btnLogout;
     private Button btnEdit;
-    private TextView etNume;
-    private TextView etAn;
-    private TextView etEmail;
-    private TextView etFacultate;
-    private TextView etUsername;
-    private TextView etPrenume;
-    private TextView etParola;
+    private Button btnSave;
+    private EditText etNume;
+    private EditText etAn;
+    private EditText etEmail;
+    private EditText etFacultate;
+    private EditText etUsername;
+    private EditText etPrenume;
+    private EditText etParola;
 
 
     public ProfilFragment() {
@@ -45,13 +47,26 @@ public class ProfilFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profil, container, false);
 
-        etNume = (TextView) view.findViewById(R.id.etLastName);
-        etAn = (TextView) view.findViewById(R.id.etYear);
-        etFacultate = (TextView) view.findViewById(R.id.etFaculty);
-        etEmail = (TextView) view.findViewById(R.id.etEmail);
-        etParola = (TextView) view.findViewById(R.id.etPassword);
-        etPrenume = (TextView) view.findViewById(R.id.etFirstName);
-        etUsername = (TextView) view.findViewById(R.id.etUsername);
+        etNume = (EditText) view.findViewById(R.id.etLastName);
+        etNume.setEnabled(false);
+
+        etAn = (EditText) view.findViewById(R.id.etYear);
+        etAn.setEnabled(false);
+
+        etFacultate = (EditText) view.findViewById(R.id.etFaculty);
+        etFacultate.setEnabled(false);
+
+        etEmail = (EditText) view.findViewById(R.id.etEmail);
+        etEmail.setEnabled(false);
+
+        etParola = (EditText) view.findViewById(R.id.etPassword);
+//        etParola.setEnabled(false);
+
+        etPrenume = (EditText) view.findViewById(R.id.etFirstName);
+        etPrenume.setEnabled(false);
+
+        etUsername = (EditText) view.findViewById(R.id.etUsername);
+        etUsername.setEnabled(false);
 
         btnLogout = (Button) view.findViewById(R.id.btnLogout);
         btnLogout.setOnClickListener(new View.OnClickListener() {
@@ -63,6 +78,40 @@ public class ProfilFragment extends Fragment {
             }
         });
 
+        btnEdit = (Button) view.findViewById(R.id.btnEdit);
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                etNume.setEnabled(true);
+                etAn.setEnabled(true);
+                etEmail.setEnabled(true);
+                etFacultate.setEnabled(true);
+               // etParola.setEnabled(true);
+                etPrenume.setEnabled(true);
+                etUsername.setEnabled(true);
+                btnEdit.setVisibility(0);
+                btnSave.setVisibility(1);
+            }
+        });
+
+        btnSave = (Button) view.findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                etNume.setEnabled(false);
+                etAn.setEnabled(false);
+                etEmail.setEnabled(false);
+                etFacultate.setEnabled(false);
+               // etParola.setEnabled(false);
+                etPrenume.setEnabled(false);
+                etUsername.setEnabled(false);
+
+                btnEdit.setVisibility(1);
+                btnSave.setVisibility(0);
+            }
+        });
         return view;
     }
 
